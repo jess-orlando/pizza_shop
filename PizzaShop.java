8e71940b4675db7a35df22307206ea8aeb2fd5e2
Jess Orlando <126181474+jess-orlando@users.noreply.github.com>
2023-06-05 14:46:53 -0400

    Initial commit

---
+++ PizzaShop.java
@@ -0,0 +1,33 @@
+// Chapter extra project
+
+// import statement for the Scanner class
+import java.util.Scanner;
+
+public class PizzaShop 
+{
+    public static void main(String[] args)
+    {
+        // Variables and constants 
+        String customer;
+        int toppings;
+        Scanner input = new Scanner(System.in);
+        Pizza pizza1 = new Pizza();
+
+        // Input phase
+        Pizza.intro();
+        System.out.print("What name will the order be under? >> ");
+        customer = input.nextLine();
+        System.out.print("Please enter the number of toppings >> ");
+        toppings = input.nextInt();
+
+        // Processing phase
+        pizza1.enterName(customer);
+        pizza1.addToppings(toppings);
+        pizza1.calcPrice();
+
+        // Output phase
+        pizza1.summary();
+        System.out.println("Order Complete!");
+
+    } // end of main()
+} // end of class
